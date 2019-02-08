e3db55e9a76cdb258250bc560f45c312293a890c
Pawel Sawicki <psawicki97@gmail.com>
2019-02-08 11:24:28 +0100

    MOD | color

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit{
- private isClickedButton = true;
+ private isClickedButton = false;
   constructor() { }
 
   ngOnInit() {
